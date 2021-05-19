7c9d16ce56da0b01930e2ed7015e83db92da3b9d
nathan hains <nathanhains14@yahoo.com>
2021-05-19 14:50:43 -0400

    commit tree 13005bcb76e46ffd8d03ba228997680c50ad4bdd

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string){
+    let noneUse = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    return string.split(' ').map(x=> {
+      return (noneUse.includes(x) === false || string.split(' ').indexOf(x) === 0 ) ? Formatter.capitalize(x) : x
+    }).join(' ')
+  }
 }
